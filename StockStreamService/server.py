274193d3c06b8b99721389e5fdd9f97a6078bb4d
Mitch-Aufiero <altermodussoftware@gmail.com>
2025-02-15 09:11:28 -0600

    adding reflections for debugging

--- StockStreamService/server.py
+++ StockStreamService/server.py
@@ -5,6 +5,8 @@ import time
 import random
 from concurrent import futures
 from datetime import datetime
+from grpc_reflection.v1alpha import reflection
+
 
 
 class StockService(stock_pb2_grpc.StockServiceServicer):
@@ -38,6 +40,14 @@ def serve():
     print("StockStreamService is booting up...")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=5)) #not sure if I need any other params here yet since they're optional
     stock_pb2_grpc.add_StockServiceServicer_to_server(StockService(), server)
+
+    service_names = (
+    stock_pb2.DESCRIPTOR.services_by_name['StockService'].full_name,
+    reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(service_names, server)
+
+
     server.add_insecure_port("0.0.0.0:50051")
 
     server.start()
